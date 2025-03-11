e2e5419abd98a09a833142c438091c6dc18d093c
Pedro Ghiotti <pedro.ghiotti.m@gmail.com>
2025-03-11 12:10:34 -0300

    feat: adicionado componente 'hello-world'

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HelloWorldComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
