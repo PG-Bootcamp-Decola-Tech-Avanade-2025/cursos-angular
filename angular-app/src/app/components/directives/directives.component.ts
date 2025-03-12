94a1338299ba7b2616120c71e5a98927a4512280
Pedro Ghiotti <pedro.ghiotti.m@gmail.com>
2025-03-12 09:54:37 -0300

    feat: adicionadas demonstrações de diretivas estruturais remanescentes (switch, for) ao componente directives

--- angular-app/src/app/components/directives/directives.component.ts
+++ angular-app/src/app/components/directives/directives.component.ts
@@ -1,18 +1,40 @@
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives',
   imports: [
-    NgIf
+    NgIf,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgFor,
+    FormsModule
   ],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.css'
 })
 export class DirectivesComponent {
   isEnabled: boolean = true;
+  currentValue: number = 0;
+  listInput: string = "";
+  list: string[] = []
 
-  toggleIsEnabled() {
+  toggleIsEnabled(): void {
     this.isEnabled = !this.isEnabled;
+  }
+
+  getMod(value: number, mod: number): number {
+    if (value == 0) return -1;
+    return Math.abs(value % mod);
+  }
+
+  pushToList(s: string): void {
+    this.list.push(s);
+  }
+
+  popFromList(index: number): void {
+    this.list.splice(index, 1);
   }
 }
